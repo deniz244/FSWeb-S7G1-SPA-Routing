6ce68097b8d051a082628acf69d4be6ef7d2e890
Deniz <77787415+deniz244@users.noreply.github.com>
2023-07-28 09:37:15 +0300

    bitti

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
+
+import { Link } from "react-router-dom";
 
 export default function KaydedilenlerListesi(props) {
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {props.list.map((movie) => (
+        <span className="saved-movie" key={movie.id}>
+          {movie.title}
+        </span>
       ))}
-      <div className="home-button">Anasayfa</div>
+      <Link to="/">
+        <div className="home-button">Anasayfa</div>
+      </Link>
     </div>
   );
 }
